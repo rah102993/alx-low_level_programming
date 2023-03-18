e1afd14626aa3e5a41531af911b3a3556736ffeb
rah102993 <raheemahlartey2012@yahoo.com>
2023-03-17 19:33:25 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints alphabets in lower cases.
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "ancdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
