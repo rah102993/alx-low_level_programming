abd5542fc976d93c6181ea0580c8c8a46fc26251
rah102993 <raheemahlartey2012@yahoo.com>
2023-03-21 14:17:25 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+/**
+ * main - a program that prints _putchar followed by a new line.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
