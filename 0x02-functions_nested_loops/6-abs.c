cf5cd768dc170d7cce3179e4c8f23b6d59036809
rah102993 <raheemahlartey2012@yahoo.com>
2023-03-21 17:46:12 -0700

    There is no such thing as absolute value in this world

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ * @c: the int to be checked
+ * Return: the absolute value int
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	return (-n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,6 @@ extern int print_alphabet_x10(void);
 extern int _islower(int c);
 extern int _isalpha(int c);
 extern int print_sign(int a);
+extern int _abs(int c);
 
 #endif
