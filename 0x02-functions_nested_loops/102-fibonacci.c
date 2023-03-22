124765bc5b1441e7d4e8d085135a9988e38d05a7
rah102993 <raheemahlartey2012@yahoo.com>
2023-03-21 19:17:10 -0700

    50 Fabonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Prints the first 50 Fibonacci numbers, starting with 1 and 2.
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int b = 0;
+	long c = 1, f = 2;
+
+	while (b < 50)
+	{
+	if (b == 0)
+	printf(", %ld", f);
+	else
+	{
+	f += c;
+	c = f - c;
+	printf(", %ld", f);
+	}
+	++b;
+	}
+	printf("\n");
+
+	return (0);
+}
+
