3682b9ae13bd19d8b4b634cfa56c370780912cd3
rah102993 <raheemahlartey2012@yahoo.com>
2023-03-21 19:37:06 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Program that finds and prints the sum of the even-valued terms.
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	long a = 1, b = 2, sum = c;
+
+	while (c + a < 4000000)
+	{
+	b += a;
+	if (c % 2 == 0)
+	sum += b;
+	a = b - a;
+	++i;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
