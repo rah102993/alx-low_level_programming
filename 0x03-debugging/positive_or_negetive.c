fffa1b5b0ff53a8aff2d957f75f46eb325016cf7
rah102993 <raheemahlartey2012@yahoo.com>
2023-03-23 15:56:06 -0700

    Debugging

--- 0x03-debugging/positive_or_negetive.c
+++ 0x03-debugging/positive_or_negetive.c
@@ -10,8 +10,6 @@
  */
 int positive_or_negative(int n)
 {
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
         /* your code goes there */
         if (n > 0)
                 printf("%d is positive\n", n);
