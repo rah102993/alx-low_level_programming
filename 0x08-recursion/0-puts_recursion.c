12ffdb4275a2eff276172aa0c78612a8d801966b
rah102993 <raheemahlartey2012@yahoo.com>
2023-04-05 15:34:43 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,19 +4,19 @@
 int _puts(const char *string)
 {
         int i = 0;
-        while(string[i])  //standard c idiom for looping through a null-terminated string
+        while(string[i])  /*standard c idiom for looping through a null-terminated string*/
         {
-                if(putchar(string[i]) == EOF)  //if we got the EOF value from writing the char
+                if(putchar(string[i]) == EOF)  /*if we got the EOF value from writing the char*/
                 {
                         return EOF;
                 }
                 i++;
         }
-        if(putchar('\n') == EOF)  //this will occur right after we quit due to the null terminated character.
+        if(putchar('\n') == EOF)  /*this will occur right after we quit due to the null terminated character.*/
         {
                 return EOF;
         }
-        return 1; //to meet spec.
+        return 1; /*to meet spec.*/
 }
 
 void _puts_recursion(char *s)
