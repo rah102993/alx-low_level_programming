a6dbe6a111cf976ec5b032c1ae58b4803ac35796
rah102993 <raheemahlartey2012@yahoo.com>
2023-04-05 15:14:44 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef HEADER_FILE_NAME
 #define HEADER_FILE_NAME
 
-int _putchar(char c)
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
