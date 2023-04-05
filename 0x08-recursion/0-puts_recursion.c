52bbddb62b39b46ac625178cfb1b8beee59b8ab5
rah102993 <raheemahlartey2012@yahoo.com>
2023-04-05 16:15:01 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,25 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
-int _puts(const char *string)
-{
-        int i = 0;
-        while(string[i])
-        {
-                if(putchar(string[i]) == EOF)
-                {
-                        return EOF;
-                }
-                i++;
-        }
-        if(putchar('\n') == EOF)
-        {
-                return EOF;
-        }
-        return 1;
-}
-
+/**
+ *
+ *@s: 
+ *
+ *
+ *
+ */
 void _puts_recursion(char *s)
 {
-	_puts(s);
+	if (*s != '\0')
+	{
+		_putchar(*);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
