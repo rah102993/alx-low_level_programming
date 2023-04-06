df9f7902401fff8b2dd0dff8c2be9617dad57522
rah102993 <raheemahlartey2012@yahoo.com>
2023-04-06 16:19:26 -0700

    4. Once an idea has taken hold of the brain

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - a function that returns the
+ *value of x raised to the power of y.
+ *@x: base
+ *@y: power
+ *Return: On Success, return the power of a number.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+		return (x);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
