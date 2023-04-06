5424a9e069cd39ec70449b641cf9118799c132b3
rah102993 <raheemahlartey2012@yahoo.com>
2023-04-06 15:55:25 -0700

    factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,31 +2,16 @@
 
 /**
  *factorial - a function that returns the factorial of a given number.
- *@n: given number
+ *@n: given number.
  *Return: Always 0 (Success)
  */
 
-int factorial(int n){
-
-	int res = 1
-	if (n == 1){
-		return 1;
-	}
-	else{
-		return n* factorial (n - 1);
-	}
-
-int main(void){
-
-	int num = 1;
-	int num2 = 5;
-	int num3 = 10;
-	int num4 = -1024;
-
-	putchar("%d/n", factorial(num1));
-	putchar("%d/n", factorial(num2));
-	putchar("%d/n", factorial(num3));
-	putchar("%d\n", factorial(num4));
-
-	return(0);
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
 }
