caf042f41b1393f5a74043d726bb9b683ad4fca9
rah102993 <raheemahlartey2012@yahoo.com>
2023-04-09 15:39:46 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: an array of arguments.
+ *Return: Always 0 (Success).
+ */
+
+int main(int argc_\ttribute_((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE_NAME
+#define HEADER_FILE_NAME
+
+int _putchar(char c)
+int _atoi(char *s);
+
+#endif
