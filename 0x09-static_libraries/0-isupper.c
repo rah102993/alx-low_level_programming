62b704f03be8af47d59e813e8d900872d30f24a2
rah102993 <raheemahlartey2012@yahoo.com>
2023-04-10 23:14:28 -0700

    static lib

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *_isupper - a program that checks whether alphabets are
- *upper case or not.
- *@c: character to check
- *Return: 0 or 1
- */
+* _isupper - a program that checks whether alphabets are
+*upper case or not.
+*@c: character to check
+*Return: 0 or 1
+*/
 
 int _isupper(int c)
 {
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,7 @@
 #ifndef HEADER_FILE_NAME
 #define HEADER_FILE_NAME
 
-int _putchar(char c)
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
