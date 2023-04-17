3f19b0624272b79aacf8862c3ce7b896fa58b9d4
rah102993 <raheemahlartey2012@yahoo.com>
2023-04-17 23:13:44 +0000

    2. File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the name of the file it was compiled from, followed by a new line.
- *
+ * main - a program that prints the name of the file
+ * it was compiled from, followed by a new line.
  * Return: Always 0 (Success).
  */
 
