586c4a2fb843bfa3fb05f71e6501197f00926cea
rah102993 <raheemahlartey2012@yahoo.com>
2023-04-19 23:16:02 +0000

    3. A goal is not always

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,17 +1,14 @@
 #ifndef CALC_H
 #define CALC_H
-
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
  */
-
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -22,3 +19,4 @@ int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif
+
