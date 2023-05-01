dcf20ae75a4c6265ab936e129a19696a77e5aa30
rah102993 <raheemahlartey2012@yahoo.com>
2023-05-01 23:29:19 +0000

    5. Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,12 +14,11 @@ void free_listint2(listint_t **head)
 
 	if (head == NULL)
 		return;
-
-	while (head != NULL)
+	while (*head != NULL)
 	{
-		*head = i;
-		i = (*head)->next;
-		free(*head);
+		i = *head;
+		*head = (*head)->next;
+		free(i);
 	}
 	*head = NULL;
 }
