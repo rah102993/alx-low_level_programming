fafb71b15251ab4bf338fa39b3ad6f4a341071bf
rah102993 <raheemahlartey2012@yahoo.com>
2023-05-09 01:18:34 +0000

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	a = open(filename, O_WRONLY | O_APPEND);
+	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	b = write(a, text_content, len);
 
 	if (a == -1 || b == -1)
