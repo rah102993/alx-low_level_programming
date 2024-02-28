645b70ca42aeb3d36f6f7cf1df17b02160607628
rah102993 <raheemahlartey2012@gmail.com>
2024-02-28 05:40:23 -0800

    Bit Manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - Returns the number of bits you would need to flip to get from one number to another.
+ * flip_bits - Returns the number of bits to flip to get next number.
  * @n: The first number.
  * @m: The second number.
  *
